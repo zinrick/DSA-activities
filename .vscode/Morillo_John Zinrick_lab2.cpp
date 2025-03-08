b81c7b2e1b13292142a5beb7b6c222f41485e1e8
zinrick <morillozinrick@gmail.com>
2025-03-08 17:49:05 +0800

    Activity 2 part 1

--- .vscode/Morillo_John Zinrick_lab2.cpp
+++ .vscode/Morillo_John Zinrick_lab2.cpp
@@ -50,7 +50,7 @@ using namespace std;
      return head;
     }
     
-    Node *inserAtBeginning(string data, Node *head){
+    Node *insertAtBeginning(string data, Node *head){
      Node *newNode = createNode(data);
      newNode->link = head;
      
@@ -60,13 +60,48 @@ using namespace std;
      
      return head;
     }
+     string insertAfter(string after, string data, Node *head){
+        Node *temp = new Node;
+        temp = head;
+        
+        while(temp->songName.compare(after) !=0){
+            if(temp == NULL){
+                return "No such song exist, please try again later.";
+            }
+            temp = temp->link;
+        }
+        Node *newNode = createNode(data);
+        newNode->link = temp->link;
+        temp->link = newNode;
+        
+        return "A new node has been added after " + after + "\n";
+     }
+    
     
 int main()
 {
   
    Node *head = createNode("Sanctuary by Joji");
   head = insertAtEnd("Sunday Morning by Maroon 5", head);
+  head = insertAtEnd("Kahit di mo na Alam by December Avenue", head);
+  head = insertAtEnd("You'll be Safe Here by Rico Blanco", head);
+  head = insertAtEnd("You are not Alone by Michael Jackson", head);
+  
+  head = insertAtBeginning("If ever you in my arms again by Peabo Bryson", head);
+  head = insertAtBeginning("Kay Tagal Kitang Hinintay by Sponge Cola", head);
+  head = insertAtBeginning("Ikaw at Ako by Johnoy Danao", head);
+  head = insertAtBeginning("The Man Who Can't be Moved by The Script", head);
+  traverse(head);
+  
+  insertAfter("Kalimutan Ka by Skusta Clee","Ikaw at Ako by Johnoy Danao", head); 
+    insertAfter("With a Smile by Eraserheads","Kay Tagal Kitang Hinintay", head); 
+  insertAfter("Your Song by Parokya ni Edgar","The Man Who Can't be Moved", head); 
+
+  
+
+  
    traverse(head);
+ 
    
 return 0;
   
